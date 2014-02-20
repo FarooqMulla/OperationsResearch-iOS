4c5980660737eaaee2f85fcf97c1b7edd5634cd3
Daniel Alvarez <i@danyalvarez.com>
2014-02-20 10:30:01 -0400

    Initial Commit

---
+++ OperationsResearch/DAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAAppDelegate.h
+//  OperationsResearch
+//
+//  Created by Daniel Alvarez on 2/20/14.
+//  Copyright (c) 2014 Daniel Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OperationsResearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OperationsResearch
+//
+//  Created by Daniel Alvarez on 2/20/14.
+//  Copyright (c) 2014 Daniel Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAppDelegate class]));
+    }
+}
